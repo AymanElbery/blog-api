aed1b23511e64a60a470ba350fae71d5bf3fbcdf
AymanElbery <ayman.bery@gmail.com>
2023-07-26 00:55:37 +0300

    enable core#6

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,11 @@ async function bootstrap() {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      "*"
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS, FETCH"
+      "*"
     );
     
     next();
